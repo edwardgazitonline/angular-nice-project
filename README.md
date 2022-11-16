50b0801c211a3b8ac7d9f320668672db5392ca05
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2022-11-16 02:31:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-h5kppg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-cmqqwp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'event-binding-with-button-one';
+  show: boolean = true;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+
+    
+  }
+  
+  onShow(): void {
+    alert('Show button clicked!');
+  }
+  
+  showHide(): boolean{
+    return this.show = false;
+  }
+}
+
+
+
+
