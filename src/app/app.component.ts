7b737d64ddf96c1d8dfa6f767ee2668467a3e841
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2022-11-16 02:55:29 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'event-binding-with-button-one';
   show: boolean = true;
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {}
 
-    
-  }
-  
+  ngOnInit(): void {}
+
   onShow(): void {
     alert('Show button clicked!');
   }
-  
-  showHide(): boolean{
-    return this.show = true;
+
+  showHide(): boolean {
+    return (this.show = false);
   }
 }
-
-
-
-
