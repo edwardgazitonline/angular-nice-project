afc1d69a5b18457eb7c309fe366dc524c3a110cc
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2023-04-22 11:38:53 +0300

    2 files modified

---
+++ src/hero/hero.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+    templateUrl: './hero.component.html',
+      styleUrls: ['./hero.component.css']
+      })
+      export class HeroComponent implements OnDestroy, OnInit, OnChanges {
+
+        @Input() name: string;
+          @Output() liked = new EventEmitter();
+
+            constructor() { }
+
+              ngOnInit(): void {
+                }
+
+                  ngOnDestroy() {
+
+                    }
+
+                      ngOnChanges(changes: SimpleChanges) {
+                         const hero = changes['name'];
+                            const oldValue = hero.previousValue;
+                               const newValue = hero.currentValue;
+
+                                  if (!hero.isFirstChange()) {
+                                      console.log(`Hero changed from ${oldValue} to ${newValue}`);
+                                         }
+                                           }
+
+                                           }
