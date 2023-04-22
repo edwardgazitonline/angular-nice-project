318bedca7e5b791ac59ebb48ec77d792beaf78a6
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2023-04-22 11:44:09 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeroComponent } from 'src/hero/hero.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent ,HeroComponentr ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
